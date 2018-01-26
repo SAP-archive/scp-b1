b6449a3af86bc00fbdae38ec2e12aad7682ad02a
Ralph <ralphrpo@gmail.com>
2018-01-26 17:24:22 +0000

    Get Items Implemented

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
 var express = require('express');
 var app = express();
 
+var sl = require('./modules/ServiceLayer'); 
+var slSession = null;
+var output = null;
+
+sl.Connect(function(error, resp){
+  if (error){
+    console.error("Can't Connect to Service Layer");
+    console.error(error);
+    return; // Abort Execution
+  } 
+  else{
+      slSession = resp;
+      slSession = slSession;
+  }
+});
+
+app.get('/GetItems', function (req, res) {
+  var options ={headers: {'Cookie': slSession.cookie}};   
+
+  sl.GetItems(options, function(error, resp){
+    if (error){
+      console.error("Can't get Items from Service Layer");
+      console.error(error);
+      return; // Abort Execution
+    } 
+    else{
+      res.send(resp);
+    }
+  });
+});
+
+
+
+
 app.get('/', function (req, res) {
-  res.send('Hello B1 Summit!! ');
+  output = 'Hello B1 Summit!! \n'
+  if (slSession)
+    output += JSON.stringify(slSession);
+ 
+  res.send(output);
 });
 
 var port = process.env.PORT || 8080
--- modules/serviceLayer.js
+++ modules/serviceLayer.js
@@ -1,24 +1,75 @@
 /* Service Layer module to interact with B1 Data */
 /* Server Configuration and User Credentials set in the /config.json file */
-
 module.exports = {
     Connect: function (response) {
         return (Connect(response));
     },
-    GetItems: function (response) {
-        return (GetItems(response));
+    GetItems: function (options, response) {
+        return (GetItems(options, response));
     },
     PostItem: function (body, response) {
         return (PostItem(body, response));
     }
 }
 
+//Load Local configuration file
+var cfg = require('../config.json');
+var SLServer = cfg.B1.Server+ ":"+cfg.ServiceLayer.Port+cfg.ServiceLayer.Path;
+
+//Load Node Modules
+var req = require('request') // HTTP Client
+
 function Connect(callback){
+    var uri = SLServer+"Login"
+    var resp = {}
+
+    //B1 Login Credentials
+    var data = {   UserName: cfg.B1.UserName,
+                    Password: cfg.B1.Password,
+                    CompanyDB:cfg.B1.CompanyDB};
+    
+    //Set HTTP Request Options
+    options = {uri: uri,body: JSON.stringify(data)}    
+    
+    console.log("Connecting to SL on "+ uri);
+    //Make Request
+    req.post(options,function(error, response, body){
+        if (!error && response.statusCode == 200) {
+            
+            body = JSON.parse(body);         
+            console.log(body)
+
+            resp.cookie = response.headers['set-cookie']
+            resp.SessionId = body.SessionId;
+        
+            return callback(null, resp);
+        } else {
+            return callback(error);
+        }
+    });
 
 }
 
-function GetItems(body,callback){
+function GetItems(options,callback){
+    var uri = SLServer  +"Items?$select=ItemCode,ItemName,"
+                        +"QuantityOnStock,QuantityOrderedFromVendors,QuantityOrderedByCustomers"
+    var resp = {}
+ 
+    //Set HTTP Request Options
+    options.uri = uri
     
+    console.log("Getting Items From SL on "+ uri);
+    
+    //Make Request
+    req.get(options,function(error, response, body){
+        if (!error && response.statusCode == 200) {      
+            body = JSON.parse(body);         
+            delete body["odata.metadata"];
+            return callback(null, body);
+        } else {
+            return callback(error);
+        }
+    });
 }
 
 function PostItem(callback){
