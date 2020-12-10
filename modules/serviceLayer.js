32bece1ca2f3b3ebfb4fa290139eca0ffbffee1d
Ralph Oliveira <ralphive@protonmail.com>
2020-12-10 12:23:53 +0000

    support api hub for items + promise

--- modules/serviceLayer.js
+++ modules/serviceLayer.js
@@ -6,11 +6,8 @@ module.exports = {
     Connect: function () {
         return Connect();
     },
-    GetItems: function (options, response) {
-        return (GetItems(options, response));
-    },
-    PostItems: function (options, body, response) {
-        return (PostItems(options, body, response));
+    GetItems: function (slOptions) {
+        return (GetItems(slOptions));
     }
 }
 
@@ -61,57 +58,60 @@ let Connect = function () {
     })
 }
 
-//Retrieve Items
-function GetItems(options, callback) {
-    var uri = SLServer + "Items?$select=ItemCode,ItemName,"
-        + "QuantityOnStock,QuantityOrderedFromVendors,QuantityOrderedByCustomers"
-        + "&$filter=ItemsGroupCode%20eq%20"+ItemGroupCode
-    var resp = {}
+//Get items from SL
+let GetItems = function (slOptions) {
+    return new Promise(function (resolve, reject) {
+        let options = {
+            method: "GET",
+            baseURL: process.env.B1_SERVER_ENV,
+            port: process.env.B1_SLPORT_ENV,
+            url: process.env.B1_SLPATH_ENV+"Items",
+            //url: process.env.B1_SLPATH_ENV,
 
-    //Set HTTP Request Options
-    options.uri = uri
-
-    console.log("Getting Items From SL on " + uri);
-
-    //Make Request
-    req.get(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            body = JSON.parse(body);
-            delete body["odata.metadata"];
-            return callback(null, body);
-        } else {
-            return callback(error);
-        }
-    });
-}
-
-//Create Items (Not implemented on the FrontEnd)
-function PostItems(options, body, callback) {
-
-    var uri = SLServer + "Items"
-    var resp = {}
-
-    //Set HTTP Request Options
-    options.uri = uri
-    body.ItemsGroupCode = ItemGroupCode
-    options.body = JSON.stringify(body);
-
-    console.log("Posting Items to SL on " + uri);
-    console.log("Item body: \n" + JSON.stringify(body));
-
-    //Make Request
-    req.post(options, function (error, response, body) {
-        if (!error && response.statusCode == 201) {
-            body = JSON.parse(body);
-            delete body["odata.metadata"];
-            console.log("ITEM CREATED - " + JSON.stringify(body))
-            return callback(null, body);
-        } else {
-            if (!error){
-                error = "Can't Create SL ITEM"
-                console.error(body)
+            headers: slOptions.headers,
+            params:{
+                $select: "ItemCode,ItemName,QuantityOnStock,QuantityOrderedFromVendors,"+
+                        "QuantityOrderedByCustomers"
             }
-            return callback(error);
         }
-    });
+
+        axios.request(options).then((response) => {
+            console.log(`SL Response: is ${response.status} - ${response.statusText}`)
+            if (response.statusCode < 200 || response.statusCode >= 300) {
+                return reject(
+                    new Error(`${response.statusCode}: ${response.req.getHeader("host")} ${response.req.path}`)
+                );
+            } else {                
+                resolve(response.data)
+            }
+        })
+        .catch((err) => {
+            console.error("Error calling B1 -" + err)
+            reject(new Error(err));
+        })
+    })
 }
+
+// //Retrieve Items
+// function GetItems(options, callback) {
+//     var uri = SLServer + "Items?$select=ItemCode,ItemName,"
+//         + "QuantityOnStock,QuantityOrderedFromVendors,QuantityOrderedByCustomers"
+//         + "&$filter=ItemsGroupCode%20eq%20"+ItemGroupCode
+//     var resp = {}
+
+//     //Set HTTP Request Options
+//     options.uri = uri
+
+//     console.log("Getting Items From SL on " + uri);
+
+//     //Make Request
+//     req.get(options, function (error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             body = JSON.parse(body);
+//             delete body["odata.metadata"];
+//             return callback(null, body);
+//         } else {
+//             return callback(error);
+//         }
+//     });
+// }
